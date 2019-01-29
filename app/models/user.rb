7fe9aa1f36825a837d48de4777378a6b2e35252e
tomohalloran@outlook.com <tomohalloran@outlook.com>
2019-01-29 16:03:34 +0000

    Added  AWS and  Stripe, billing functionality, API keys, and photo uploads.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  has_one :payment
+  belongs_to :user
+
+  accepts_nested_attributes_for :payment
+         
 end
